9dcfef8940467c8fbbe2790ab313b6009cffb6ee
Bart Cielinski <bcielinski@gmail.com>
2018-10-28 12:53:54 +0000

    highlight headers and refactor

--- src/Edmonds/ConsolePrinter.php
+++ src/Edmonds/ConsolePrinter.php
@@ -6,6 +6,11 @@ class ConsolePrinter
 {
     private $mt;
 
+    const COL_DIVIDER_CHAR = "|"; 
+    const ROW_DIVIDER_CHAR = "―"; 
+    const COLOR_START = "\033[1;32m";
+    const COLOR_END = "\033[0m";
+
     public function __construct(MultiplicationTable $mt)
     {
         $this->mt = $mt;
@@ -13,21 +18,28 @@ class ConsolePrinter
 
     public function print()
     {
-        $table = $this->mt->getValues();
-        $width = strlen(count($table) * count($table));
-        $rowLength = count($table) * ($width + 1) + 1;
-        $line = str_repeat("=", $rowLength) . "\n";
-        foreach ($table as $row) {
-            echo $line;
-            echo "|";
-            foreach ($row as $cell) {
-                echo str_pad($cell, $width, " ", STR_PAD_LEFT);
-                echo "|";
+        $values = $this->mt->getValues();
+        $colWidth = strlen(count($values) * count($values));
+
+        foreach ($values as $rowNum => $row) {
+            foreach ($row as $colNum => $col) {
+                $cell = str_pad($col, $colWidth, " ", STR_PAD_LEFT) . self::COL_DIVIDER_CHAR;
+                if ($rowNum === 1 || $colNum === 0) {
+                    echo $this->getColoredString($cell);
+                } else {
+                    echo $cell;
+                }
             }
             echo "\n";
+            if ($rowNum === 1) {
+                $divider = " " . str_repeat(self::ROW_DIVIDER_CHAR, (count($values) * ($colWidth + 1) - 1)) . "\n";
+                echo $this->getColoredString($divider);
+            }
         }
-        echo $line;
+    }
+
+    private function getColoredString(string $string) 
+    {
+        return self::COLOR_START . $string . self::COLOR_END;
     }
 }
-
-
